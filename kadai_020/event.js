fcd9d7789bd2d373f49d475e6ea4e5ee7640e0a0
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-18 10:45:10 +1000

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById('btn');
- 
 
-btn.addEventListener('click', () => {
-  console.log('ボタンをクリックしました');
+btn.addEventListener('click', ()=> {
+  
+setTimeout(() => {
+    const text = document.getElementById('text');
+    text.textContent = "ボタンをクリックしました";
+  }, 2000);
+
 });
